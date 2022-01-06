8cc43a9955986b02a68b7ba7391f9967209cc890
Gustavo Simão Zamai <gustavosimaozamai@gmail.com>
2022-01-06 19:09:54 -0300

    Lesson 03
    Initialize validation of age

--- src/script.php
+++ src/script.php
@@ -9,6 +9,30 @@
     $age = $_POST['age'];
     //var_dump($age);
     
+    //validation of $name
+    if (empty($name)){
+        echo 'Name cant be empty!';
+        return;
+    }
+
+    if (strlen($name) < 3){
+        echo 'Name must have contain 3 character';
+        return;
+    }
+    if (strlen($name) > 35){
+        echo 'Name is too lengthy';
+        return;
+    }
+
+    //validation of $age
+    if (!is_numeric($age)){
+        echo 'Invalid age';
+        return;
+    }
+
+
+
+
     if ($age >= 6 && $age <= 12){
         for ($x = 0; $x <= count ($categories); $x++){
             if ($categories == 'Children'){
